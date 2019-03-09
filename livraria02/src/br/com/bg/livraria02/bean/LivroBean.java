372c0910e0c20e5ba8a419a95189c75bf24db44e
rubens leme <rubenss.leme@gmail.com>
2019-03-08 22:16:01 -0300

    Projeto inicial de utilização educacional no BG.

---
+++ livraria02/src/br/com/bg/livraria02/bean/LivroBean.java
@@ -0,0 +1,12 @@
+package br.com.bg.livraria02.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	
+	public void gravarLivro() {
+		System.out.println("Gravando Livro !");
+	}
+}
---
+++ teste/src/teste/Teste.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Teste {
+	
+
+	public static void main(String[] args) {
+		boolean gravado;
+		int valor ;
+		gravado = false;
+		valor = Integer.parseInt("5");
+
+	}
+
+}
